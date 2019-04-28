72cfe1cd9c3981e4a1692bcffde4a04deaccd47f
yangtao <1509440167@qq.com>
2019-04-28 18:40:34 +0800

    www

--- js/index.js
+++ js/index.js
@@ -103,7 +103,6 @@ $(document).ready(function () {
     // 点击首页开始按钮
     $('#start').on('click',function(){
         $('.home').hide();
-        $(".enter").css("height", $(document).height());
         $('.enter').show();
     });
     // 选择
