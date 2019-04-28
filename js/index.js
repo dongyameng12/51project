7cebdbba79892c0c40c0dba355170c9de0876c5f
yangtao <1509440167@qq.com>
2019-04-28 18:21:40 +0800

    的生产者

--- js/index.js
+++ js/index.js
@@ -496,10 +496,7 @@ function getQueryVariable(variable)
        var vars = query.split("&");
        for (var i=0;i<vars.length;i++) {
                var pair = vars[i].split("=");
-               if(pair[0] == variable){
-                //    return pair[1];
-                    return(true)
-                }
+               if(pair[0] == variable){return pair[1];}
        }
        return(false);
 }
